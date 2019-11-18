60f93c4238851d7853d1222dd80f26f0e579e207
Francesca Palamara <fipalamara@gmail.com>
2019-11-18 12:52:53 -0800

    FirstPrototype Riscue
    PlayerController and CameraController are implemented

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //player variable assigned to calling PlayerController script 
+    public PlayerController thePlayer; 
+
+    //store Z position of Player in world 
+    private Vector3 lastPlayerPosition;  
+
+    //move camera by certain amount as player is moving 
+    private float distanceToMove; 
+    
+    void Start()
+    {
+        //method of how to find PlayerController component in hierarchy
+        thePlayer = FindObjectOfType<PlayerController>();
+
+        //last player position is position of where player is in that moment in time
+        //grab xyz values of player (via playercontroller) and set them to lastplayerposition vector variable
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //difference between player position last time and it's current position 
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;  
+
+        //position of camera is set to new x position value listed up above^ 
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);  
+        //we are solely adjusting x value of camera position (horizontally) not it's y or z values
+        //this is important for feel of following character 
+
+        //last player position is position of where player is in that moment in time
+        //grab xyz values of player (via playercontroller) and set them to lastplayerposition vector variable
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider; 
+
+    private Animator myAnimator; 
+
+    void Start()
+    {
+        //grab Rigidbody2D component from Player 
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        //grab Box Collider component from Player
+        myCollider = GetComponent<Collider2D>(); 
+
+        //grab Animator component from Player 
+        myAnimator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //return true or false if collider is touching another collider on different layer
+        //aka is player collider touching ground collider (different layers)  
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        //grab speed of Player and set to new x value moveSpeed and same y value speed
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        //if you press up arrow, W or hold mouse down 
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded) //is true 
+            {
+                //set Player's speed to a new jumping value 
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+
+        //grab speed value of Player and set it to Speed variable in Animator 
+        myAnimator.SetFloat ("Speed", myRigidbody.velocity.x); 
+
+        //set grounded boolean (layer checker) to Grounded boolean in Animator 
+        myAnimator.SetBool ("Grounded", grounded); 
+    }
+}
