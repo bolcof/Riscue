9d970ac3c4af0d1f65dcd81ca7fea73fd07f5fad
Francesca Palamara <fipalamara@gmail.com>
2019-11-19 15:54:38 -0800

    Merging projects!!

--- Assets/Scripts/BackGroundController.cs
+++ Assets/Scripts/BackGroundController.cs
@@ -18,6 +18,13 @@ public class BackGroundController : MonoBehaviour
         {
             addedLayerNum[i] = 0;
         }
+        
+        for (int i = 0; i < layerNUM; i++)
+        {
+            GameObject tmpObj = Instantiate(layer[i], new Vector3(layer[i].transform.position.x + layerImageWidth[i] * (addedLayerNum[i] + 1), layer[i].transform.position.y, layer[i].transform.position.z), Quaternion.identity, this.transform);
+            tmpObj.transform.parent = layer[i].transform;
+            addedLayerNum[i]++;
+        }
     }
 
     // Update is called once per frame
