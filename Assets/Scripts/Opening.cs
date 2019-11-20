e73d16caabd32188853f50b7983fea37d4d259a8
bolcof <sanctum.crusade.ex@gmail.com>
2019-11-19 16:51:17 -0800

    making opening

---
+++ Assets/Scripts/Opening.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Opening : MonoBehaviour
+{
+    public float timer;
+    public Button playButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if(timer >= 0)
+        {
+
+        }
+    }
+
+    public void ClickPlay() {
+        SceneManager.LoadScene("Prototype3");
+    }
+}
