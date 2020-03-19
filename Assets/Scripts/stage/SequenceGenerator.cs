9107981eda63b8287fd93a411780d5302bd5baaf
bolcof <sanctum.crusade.ex@gmail.com>
2020-03-20 07:51:30 +0900

    made stage2

--- Assets/Scripts/stage/SequenceGenerator.cs
+++ Assets/Scripts/stage/SequenceGenerator.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class SequenceGenerator : MonoBehaviour
 {
+    public int level;
     public GameObject[] sequence = new GameObject[8];
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < sequence.Length+1; i++) {
-            string fileNum = i.ToString("D2");
-            sequence[i-1] = (GameObject)Resources.Load("Prefabs/Sequence/easy/Sequence_e" + fileNum);
+        if(level == 1){
+            for (int i = 1; i < sequence.Length+1; i++) {
+                string fileNum = i.ToString("D2");
+                sequence[i-1] = (GameObject)Resources.Load("Prefabs/Sequence/easy/Sequence_e" + fileNum);
+            }
+        }else{
+            for (int i = 1; i < sequence.Length+1; i++) {
+                string fileNum = i.ToString("D2");
+                sequence[i-1] = (GameObject)Resources.Load("Prefabs/Sequence/normal/Sequence_n" + fileNum);
+            }
         }
         for (int j = 0; j < 30; j++)
         {
